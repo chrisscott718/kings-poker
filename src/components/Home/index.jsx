940484f91481c6ad7a699dca911ff361a77fd17f
Chris Scott <chris.scott718@gmail.com>
2018-04-07 17:59:12 -0700

    add footer/style footer

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -10,23 +10,27 @@ import {Button} from 'Shared/Button';
 export default class Home extends Component {
   render() {
     return (
-      <main className="wrapper d-flex f-a-center f-j-center ta-center">
-        <div className="header-content">
-          <h1 className="mb4 mt0">Redefine your Poker Experience</h1>
-          <p className="lead">Custom Luxury Poker Tables Built to Perfection</p>
-          <span className="seperator"></span>
-          <Button className="mb4">
-            <NavLink to="/buildyourown">
-              Build Your Own
-            </NavLink>
-          </Button>
-          <div className="call-us">
-            <p className="t-sm t-muted mb1">or talk to a real person</p>
-            <a className="c-gold t-bold td-none t-md" href="tel:1-800-897-1189">800-897-1189</a>
+      <div>
+        <main className="wrapper d-flex f-a-center f-j-center ta-center">
+          <div className="header-content">
+            <h1 className="mb4 mt0">Redefine your Poker Experience</h1>
+            <p className="lead">Custom Luxury Poker Tables Built to Perfection</p>
+            <span className="seperator"></span>
+            <Button className="mb4">
+              <NavLink to="/buildyourown">
+                Build Your Own
+              </NavLink>
+            </Button>
+            <div className="call-us">
+              <p className="t-sm t-muted mb1">or talk to a real person</p>
+              <a className="c-gold t-bold td-none t-md" href="tel:1-800-897-1189">800-897-1189</a>
+            </div>
           </div>
-        </div>
-        <img src={headerImage} alt="Kings Poker Table Round" className="header-img"/>
-      </main>
+          <img src={headerImage} alt="Kings Poker Table Round" className="header-img"/>
+        </main>
+        <footer className="t-sm t-muted ta-center">Copyright © 2018 Kings Poker Tables</footer>
+      </div>
+
     );
   }
 }
