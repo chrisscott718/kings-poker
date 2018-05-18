dfb158404b3328baae364fa1e61001281543ba29
chrisscott718 <chris.scott718@gmail.com>
2018-05-17 20:05:24 -0700

    add mobile styles for home and navigation

--- react-ui/src/components/Home/index.jsx
+++ react-ui/src/components/Home/index.jsx
@@ -4,6 +4,7 @@ import {NavLink} from 'react-router-dom';
 import './style.scss';
 
 import headerImage from 'Images/header-img.png';
+import logo from 'Images/logo-center.png'
 
 import {Button} from 'Shared/Button';
 import {Footer} from 'Shared/Footer';
@@ -14,6 +15,7 @@ export default class Home extends Component {
       <div>
         <main className="wrapper d-flex f-j-center f-a-center ta-center">
           <div className="header-content">
+            <img width="125" src={logo} className="logo-mobile" alt="Kings Poker Tables | Luxury Custom Poker Tables"/>
             <h1 className="mb4 mt0">Redefine your Poker Experience</h1>
             <p className="lead">Custom Luxury Poker Tables Built to Perfection</p>
             <span className="seperator"></span>
--- react-ui/src/components/Navigation/index.jsx
+++ react-ui/src/components/Navigation/index.jsx
@@ -6,6 +6,24 @@ import './style.scss';
 import logo from 'Images/logo-h.png';
 
 export default class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navIsOpen: false,
+    };
+
+    this.toggleNavigation = this.toggleNavigation.bind(this);
+  }
+
+  toggleNavigation() {
+    this.setState((prevState) => {
+      return {
+        navIsOpen: !prevState.navIsOpen
+      };
+    });
+  }
+
   render() {
     return (
       <nav>
@@ -13,23 +31,35 @@ export default class Navigation extends Component {
           <div className="nav-wrapper">
             <div className="nav-brand">
               <NavLink to="/">
-                <img width="280" src={logo} alt="Kings Poker Tables | Luxury Custom Poker Tables"/>
+                <img width="280" src={logo} className="logo-large" alt="Kings Poker Tables | Luxury Custom Poker Tables"/>
               </NavLink>
             </div>
+            <div className="nav-control">
+              <div className={`${this.state.navIsOpen ? 'open' : '' } menu hamburger`} onClick={this.toggleNavigation}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
             <div className="nav-content">
-              <div className="nav">
+              <div className={`${this.state.navIsOpen ? 'open' : '' } nav`}>
                 <ul className="nav-links">
-                  <li className="nav-link">
+                  <li className="nav-link" onClick={this.toggleNavigation}>
+                    <NavLink to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-link" onClick={this.toggleNavigation}>
                     <NavLink to="/buildyourown">
                       Build A Table
                     </NavLink>
                   </li>
-                  <li className="nav-link">
+                  <li className="nav-link" onClick={this.toggleNavigation}>
                     <NavLink to="/about">
                       About
                     </NavLink>
                   </li>
-                  <li className="nav-link">
+                  <li className="nav-link" onClick={this.toggleNavigation}>
                     <NavLink to="/contact">
                       Contact
                     </NavLink>
